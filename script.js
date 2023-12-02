d964f3e8af4063fca73ca544e0e0e7f4ecf1ef35
Aklaikel <anas.klaikel95@gmail.com>
2023-12-02 02:04:55 +0100

    add a local storage for the refresh

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputBox = document.getElementById("todo-input");
 const listContainer = document.getElementById("list-container");
 
+loadList();
 function addTask() {
   if (inputBox.value === '')
     alert("Please enter a task");
@@ -14,12 +15,23 @@ function addTask() {
         li.appendChild(span);
     }
     inputBox.value = "";
+    saveList();
 }
 
 listContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
+    saveList();
   } else if (e.target.tagName === "SPAN") {
     e.target.parentElement.remove();
+    saveList();
   }
 }, false);
+
+function saveList() {
+  localStorage.setItem("listContainer", listContainer.innerHTML);
+}
+
+function loadList() {
+    listContainer.innerHTML = localStorage.getItem("listContainer");
+}
