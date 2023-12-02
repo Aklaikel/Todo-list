fa90556c6c9153ad6fce666312aa8d2166d11dd6
Aklaikel <anas.klaikel95@gmail.com>
2023-12-02 01:59:07 +0100

    fix:if i click cross the task will be removed

--- script.js
+++ script.js
@@ -20,6 +20,6 @@ listContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
   } else if (e.target.tagName === "SPAN") {
-    e.target.addEventListener.remove();
+    e.target.parentElement.remove();
   }
 }, false);
